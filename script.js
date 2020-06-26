e4a88e43ee7827b1bfd767e5da1ce1c311d85d41
Elaine Shaw <57796267+elshaw82@users.noreply.github.com>
2020-06-26 14:57:16 +0100

    more styles

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menuButton = document.querySelector(".hamburger-menu");
 const navLinks = document.querySelector(".nav-links");
+const eachLink = navLinks.querySelectorAll(".link");
+
 var menuOpen = false;
 
 menuButton.addEventListener('click', () =>{
@@ -7,11 +9,26 @@ menuButton.addEventListener('click', () =>{
      menuButton.classList.add('open');
      menuOpen = true;
      navLinks.classList.add('mobile-toggle')
-     navLinks.classList.add('pointer-events')
  } else {
      menuButton.classList.remove('open');
      menuOpen = false;
      navLinks.classList.remove('mobile-toggle')
-     navLinks.classList.remove('pointer-events')
  }
 })
+
+for( var i = 0; i < eachLink.length; i++){
+    eachLink[i].addEventListener('click', closeMenu)
+}
+
+function closeMenu(){
+    if(!menuOpen) {
+     menuButton.classList.add('open');
+     menuOpen = true;
+     navLinks.classList.add('mobile-toggle')
+ } else {
+     menuButton.classList.remove('open');
+     menuOpen = false;
+     navLinks.classList.remove('mobile-toggle')
+ }
+}
+
